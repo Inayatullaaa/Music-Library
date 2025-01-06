27b78040811759041ac7642caf268e3652f57c97
Inayatulla <241637@astanait.edu.kz>
2025-01-06 21:57:11 +0500

    first

--- src/Project/MusicItem.java
+++ src/Project/MusicItem.java
@@ -7,36 +7,3 @@ public abstract class MusicItem {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Narration: the Track class extends the Song class
-//public class Track extends Song {
-//    private String narration;       // For podcasts or audiobooks, e.g.
-//
-//    public Track(String songName, Artist artist, String songDuration, String songGenre, String narration) {
-//        super(songName, artist, songDuration, songGenre);  // Call the constructor of the parent class
-//        this.narration = narration;
-//    }
-//
-//    // Overriding the toString method to output additional information
-//    @Override
-//    public String toString() {
-//        return super.toString() + "Narration=" + narration;
-//    }
-//}
